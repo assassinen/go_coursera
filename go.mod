66eee6935b455f5fa06dc2d2287f2e680d306c6c
inovikov <i.novikov@city-mobil.ru>
2021-09-17 21:05:29 +0300

    go.mog

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/assassinen/go_coursera
+
+go 1.14
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-	//"github.com/assassinen/go_coursera/hello"
+	"github.com/assassinen/go_coursera/hello"
 )
 
 func main() {
-	//Hello()
-	fmt.Println("Hello, Go!")
+	hello.Hello()
 }
